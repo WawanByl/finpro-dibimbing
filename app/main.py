a1d3e531379b7d11a5362adca8ceab6595b32e77
WawanByl <iw4nbyl@gmail.com>
2024-08-17 17:40:08 +0700

    tambah gcloudignore

--- app/main.py
+++ app/main.py
@@ -1,9 +1,30 @@
 from flask import Flask, request, jsonify
 import os
+import pandas as pd
 from models.model import create_baseline_model
 from data.preprocessing import load_data, preprocess_data, split_and_scale_data
+from google.cloud import storage
 
 app = Flask(__name__)
+
+# Fungsi untuk upload dan download dari Google Cloud Storage
+def upload_file_to_bucket(file_path, destination_blob_name):
+    """Upload file to a bucket"""
+    bucket_name = 'final_project_lstm'
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(file_path)
+    print(f"File {file_path} uploaded to {destination_blob_name}.")
+
+def download_file_from_bucket(source_blob_name, destination_file_name):
+    """Download file from a bucket"""
+    bucket_name = 'final_project_lstm'
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+    print(f"File {source_blob_name} downloaded to {destination_file_name}.")
 
 @app.route('/predict', methods=['POST'])
 def predict():
